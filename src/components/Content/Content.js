ef7f1aa8716459877445edd417342cf00afb8f9f
IM <mizi@d-tt.nl>
2019-11-22 17:44:33 +0000

    Final touch ups

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,12 +1,12 @@
 import React from 'react';
- 
-const content = (props) => (
 
-   <div className="movie">
-         <div onClick={props.details} className="content__column-poster">
+const contentRow = (props) => (
+
+    <div className="movie">
+        <div onClick={props.details} className="content__column-poster">
             <img src={props.image} alt="" className="content__poster" />
-         </div>
-   </div> 
+        </div>
+    </div>
 );
 
-export default content;
+export default contentRow;
